5a96ed47c08b18f0411cb6d34d915de352243ef6
stretchstretchy <72479096+stretchstretchy@users.noreply.github.com>
2026-08-01 17:22:29 +1000

    create latest release with esp firmware check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.6"
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
